5bfe215f6fb9067cec1f75ab709108bf4ef626bb
Simon <simon@Simons-iMac.local>
2020-12-20 20:46:00 -0500

    json

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from collections import Counter as ctr
-a = "aaaaaadkfsssdddd"
-mycounter = ctr(a)
-print(mycounter.items())
+import json
+
+person = {"name": "John", "age": 30, "hasChildren": True, "titles": ["engineer", "programmer"]}
+personJSON = json.dumps(person, indent=4, sort_keys=True)
+print(personJSON)
+
+with open("person.json", "w") as jsFile:
+    json.dump(person, jsFile, indent=4)
+
+newPerson = json.loads(personJSON)
+print(newPerson)
