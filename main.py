e75d1d4d206077b47db37f78b8deaeceb043cf4c
Simon <simon@Simons-iMac.local>
2021-01-02 21:03:28 -0500

    copy2()

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-from shutil import copyfile, copyfileobj
+from shutil import copyfile, copyfileobj, copy2
 from sys import exit
 import os
 import shutil
 
 source = "/Users/simon/pyproj/proj1/binding.mp4"
-target = "/Users/simon/pyproj/proj1/binding-copyfileobj.mp4"
-
+target = "/Users/simon/pyproj/proj1/binding-copy2.mp4"
+fsrc = open(source, "r")
+fdst = open(target, "w")
 try:
-    #copyfile(source, target) #read the entire file in one go by default, copy only the data
-    #shutil.copy(source, target) #copy with permissions
-    shutil.copyfileobj(source, target)
+    """
+    1.
+    copyfile(source, target) #read the entire file in one go by default, copy only the data
+    2.
+    shutil.copy(source, target) #copy with permissions
+    3.
+    with open(source, "rb") as f_in, open(target, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
+    """
+    copy2(source, target)
 except IOError as err:
     print(f"Unable to copy file {source} due to error {err}")
     exit(1)
 except:
-    print(f"Unexpected error: {Error}")
+    print(f"Unexpected error")
     exit(1)
 print("File copy done")
